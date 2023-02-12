621a4f90b47dea7899f2efa15947f7f675f50e42
John <kami25565@gmail.com>
2023-02-12 14:00:48 +0800

    feat: add form translate

--- translate.ts
+++ translate.ts
@@ -15,8 +15,13 @@ function zh(str: string): string {
     case "Galar": return "伽勒爾"
     case "Paldea": return "帕底亞"
     case "Hisui": return "洗翠"
+    case "Combat Breed": return "鬥戰種"
     case "Blaze Breed": return "火熾種"
     case "Aqua Breed": return "水瀾種"
+    case "Zen Mode": return "達摩模式"
+    case "Red-Striped": return "紅條紋"
+    case "Blue-Striped": return "藍條紋"
+    case "White-Striped": return "白條紋"
   }
 
   return str
@@ -28,8 +33,13 @@ function jp(str: string): string {
     case "Galar": return "ガラル"
     case "Paldea": return "パルデア"
     case "Hisui": return "ヒスイ"
+    case "Combat Breed": return "コンバットしゅ"
     case "Blaze Breed": return "ブレイズしゅ"
     case "Aqua Breed": return "ウォーターしゅ"
+    case "Zen Mode": return "ダルマモード"
+    case "Red-Striped": return "あかすじのすがた"
+    case "Blue-Striped": return "あおすじのすがた"
+    case "White-Striped": return "しろすじのすがた"
   }
 
   return str
